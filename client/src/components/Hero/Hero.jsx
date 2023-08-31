8974feac87f22736f85885ac4e9e184392f9fa5b
anesu398 <ndabaprinco@gmail.com>
2023-08-31 20:31:10 +0200

    feelathome

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,84 @@
+import "./Hero.css";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
+import SearchBar from "../SearchBar/SearchBar";
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        {/* left side */}
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            
+            <motion.h1
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            
+            ><div id="card"/>
+              The journey <br /> 
+              towards finding your 
+              <br /> perfect home begins.
+            </motion.h1>
+            
+          </div>
+          
+          <div id="rect"/>
+          <div className="flexColStart secondaryText flexhero-des">
+         
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+            <div id="circle"></div>
+           
+          </div>
+
+          <SearchBar/>
+
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Product</span>
+            </div>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={200} duration={4} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customer</span>
+            </div>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={2} /> <span>+</span>
+              </span>
+              <span className="secondaryText">Awards Winning</span>
+            </div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flexCenter hero-right">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            className="image-container"
+          >
+            <img src="./hero-image.png" alt="houses" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ client/src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "with the rates going up, the number of buyers has decreased, leading to less demand for properties. However we are hapy to tell you that we got you."
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "We help you with the information on where and when to sell your property"
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "with us, you always get what you hoped for."
+      },
+];
+export default data;
