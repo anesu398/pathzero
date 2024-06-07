1746c49c41ebcbc0dbf221b0719b8fa171a7db46
anesu398 <ndabaprinco@gmail.com>
2024-06-07 22:08:16 +0200

    jjh

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "No token provided, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
